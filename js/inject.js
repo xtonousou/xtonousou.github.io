6c4127eab8c5bee26ad488110a44274129dfbd31
Sotiris Roussis <xtonousou@gmail.com>
2017-02-26 00:42:43 +0200

    lets see about that one

--- js/inject.js
+++ js/inject.js
@@ -1,14 +1,25 @@
-// must be called after #console on the .html
-
+alert(document.title);
 switch (document.title) {
 
   case "/home/xtonousou":
-    document.getElementById('cdh1').onclick = "appendCommand('cd bio/ '); window.location='/bio.html'";
-    document.getElementById('cdh2').onclick = "appendCommand('cd contact/ '); window.location='/contact.html'";
-    document.getElementById('cdh3').onclick = "appendCommand('cd projects/ '); window.location='/projects.html'";
-    document.getElementById('al').onclick = "appendCommand('$BROWSER https://www.archlinux.org '); window.location='https://www.archlinux.org'";
-    document.getElementById('pk').onclick = "appendCommand('wget -q /xtonousou-pubkey.asc '); window.location='/xtonousou-pubkey.asc'";
-    
+    console.log("im in");
+    var bio = document.getElementById('cdh1');
+    var contact = document.getElementById('cdh2');
+    var projects = document.getElementById('cdh3');
+    var distro = document.getElementById('al');
+    var publickey = document.getElementById('pk');
+
+    bio.href = "/bio.html";
+    contact.href = "/contact.html";
+    projects.href = "/projects.html";
+    distro.href = "https://www.archlinux.org";
+    publickey.href = "/xtonousou-pubkey.asc";
+
+    bio.onclick = appendCommand('cd bio/ ');
+    contact.onclick = appendCommand('cd contact/ ');
+    projects.onclick = appendCommand('cd projects/ ');
+    distro.onclick = appendCommand('$BROWSER https://www.archlinux.org ');
+    publickey.onclick = appendCommand('wget -q /xtonousou-pubkey.asc ');    
     break;
   case "/home/xtonousou/bio":
 
