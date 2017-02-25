560fd87cf0872f682beebbef2356558c8833288a
Sotiris Roussis <xtonousou@gmail.com>
2017-02-25 21:03:58 +0200

    testing shit

--- js/typer.js
+++ js/typer.js
@@ -31,8 +31,6 @@ var Typer = {
   },
  
   write: function(str) { // append to console content
-
-    alert(str);
     
     $("#console").append(str);
     return false;
@@ -93,9 +91,10 @@ var Typer = {
           Typer.index -= Typer.speed; //remove speed for deleting text
       }
       var text = Typer.text.substring(0, Typer.index); // parse the text for stripping html entities
-      var rtn = new RegExp("\n", "g"); // newline regex
+
+      alert(text);
   
-      $("#console").html(text.replace(rtn,"<br/>")); // replace newline chars with br, tabs with 4 space and blanks with an html blank
+      $("#console").html(text.replace(new RegExp("\n", "g"),"<br/>")); // replace newline chars with br, tabs with 4 space and blanks with an html blank
       window.scrollBy(0, 50); // scroll to make sure bottom is always visible
     }
     
