f99d700b2e552788bb77e945cff07ed511958461
Sotiris Roussis <xtonousou@gmail.com>
2017-02-25 02:39:47 +0200

    Sadreacts Only

--- js/crt.js
+++ js/crt.js
@@ -1,51 +1,23 @@
-// Here I load a PNG with scanlines.
-// Start loading the image right away, not after the onload event.
-var lines = new Image();
-lines.src = 'http://i.imgur.com/TAJ0Zkw.png';
+$(document).ready(function() {
+  
+  var img = $(".glitch");
+  var prefix = "data:image/jpg;base64,";
+  var maxErrors = 200;
 
-window.addEventListener('load', fakeCRT, false);
+  function update() {
+    
+    var corrupted = data;
+    if (Math.random() > 0.8) {
+      var errors = Math.round(Math.random() * maxErrors)
+      for (var i = 0; i < errors; i++) {
+        var l = 2000 + Math.round(Math.random() * (corrupted.length - 2002));
+        corrupted = corrupted.substr(0, l) + corrupted.charAt(l + 1) + corrupted.charAt(l) + corrupted.substr(l + 2);
+      }
+    }
+    img.attr("src", prefix + corrupted);
 
-function fakeCRT() {
-  var glcanvas, source, srcctx, texture, w, h, hw, hh, w75;
-  
-  // Try to create a WebGL canvas (will fail if WebGL isn't supported)
-  try {
-    glcanvas = fx.canvas();
-  } catch (e) {return;}
-  
-  // Assumes the first canvas tag in the document is the 2D game, but
-  // obviously we could supply a specific canvas element here.
-  source = document.getElementsByTagName('canvas')[0];
-  srcctx = source.getContext('2d');
-  
-  // This tells glfx what to use as a source image
-  texture = glcanvas.texture(source);
-  
-  // Just setting up some details to tweak the bulgePinch effect
-  w = source.width;
-  h = source.height;
-  hw = w / 2;
-  hh = h / 2;
-  w75 = w * 0.75;
-  
-  // Hide the source 2D canvas and put the WebGL Canvas in its place
-  source.parentNode.insertBefore(glcanvas, source);
-  source.style.display = 'none';
-  glcanvas.className = source.className;
-  glcanvas.id = source.id;
-  source.id = 'old_' + source.id;
-  
-  setInterval(function () {
-    // Give the source scanlines
-    srcctx.drawImage(lines, 0, 0, w, h);
-  
-    // Load the latest source frame
-    texture.loadContentsOf(source);
-  
-    // Apply WebGL magic
-    glcanvas.draw(texture)
-      .bulgePinch(hw, hh, w75, 0.12)
-      .vignette(0.25, 0.74)
-      .update();
-  },ath.floor(1000 / 40));
-}
+  }
+  update();
+  setInterval(update, 26);
+
+});
