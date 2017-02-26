d55d1836988bd6d78ed0415ee10f8ec83f81779e
Sotiris Roussis <xtonousou@gmail.com>
2017-02-26 02:37:57 +0200

    omfg test

--- js/inject.js
+++ js/inject.js
@@ -8,17 +8,17 @@ switch (document.title) {
     var distro = document.getElementById('al');
     var publickey = document.getElementById('pk');
 
+    //bio.href = "/bio.html";
+    contact.href = "/contact.html";
+    projects.href = "/projects.html";
+    distro.href = "https://www.archlinux.org";
+    publickey.href = "/xtonousou-pubkey.asc";
+
     bio.onclick = function() { appendCommand('cd bio/ '); }
     contact.onclick = function() { appendCommand('cd contact/ '); }
     projects.onclick = function() { appendCommand('cd projects/ '); }
     distro.onclick = function() { appendCommand('$BROWSER https://www.archlinux.org '); }
     publickey.onclick = function() { appendCommand('wget -q /xtonousou-pubkey.asc '); }
-
-    bio.href = "/bio.html";
-    contact.href = "/contact.html";
-    projects.href = "/projects.html";
-    distro.href = "https://www.archlinux.org";
-    publickey.href = "/xtonousou-pubkey.asc";
     
     break;
   case "/home/xtonousou/bio":
