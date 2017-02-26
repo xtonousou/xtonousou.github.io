86361cc2241bac06882facc47da098030a74fdfe
Sotiris Roussis <xtonousou@gmail.com>
2017-02-26 03:15:51 +0200

    moved appendCommand to injector

--- js/inject.js
+++ js/inject.js
@@ -14,7 +14,7 @@ switch (document.title) {
     distro.href = "https://www.archlinux.org";
     publickey.href = "/xtonousou-pubkey.asc";
 
-    $("#cdh1").click(function() { appendCommand('cd bio/ '); });
+    bio.onclick = function() { appendCommand('cd bio/ '); }
     contact.onclick = function() { appendCommand('cd contact/ '); }
     projects.onclick = function() { appendCommand('cd projects/ '); }
     distro.onclick = function() { appendCommand('$BROWSER https://www.archlinux.org '); }
@@ -35,4 +35,25 @@ switch (document.title) {
     break;
   default:
     console.error("inject.js : unknown document.title");
+}
+
+// NOTE: if the text you want to append is "cd path/" pass "cd path/ " instead (an extra space)
+function appendCommand(command) {
+
+  Typer.text = command; // save it in Typer.text
+  Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+  if (Typer.content().substring(Typer.content().length - 1, Typer.content().length) == "_") // if the last char is the blinking cursor
+    $("#console").html($("#console").html().substring(0, Typer.content().length - 1)); // remove it before adding the text
+  $("#console").append(Typer.text);
+  window.scrollBy(0, 50); // scroll to make sure bottom is always visible
+
+  var timer2 = setInterval( function t2() {
+    if (Typer.text && Typer.index > Typer.text.length) {
+      clearInterval(timer2);
+      $("#console").html($("#console").html().substring(0, Typer.content().length - Typer.text.length)); // clear before change directory
+      Typer.updLstChr();
+    }
+  }, 750);
+  
+  return true;
 }
--- js/typer.js
+++ js/typer.js
@@ -134,24 +134,3 @@ function initTyper(typingSpeed, textFile) {
   
   return true;
 }
-
-// NOTE: if the text you want to append is "cd path/" pass "cd path/ " instead (an extra space)
-function appendCommand(command) {
-
-  Typer.text = command; // save it in Typer.text
-  Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-  if (Typer.content().substring(Typer.content().length - 1, Typer.content().length) == "_") // if the last char is the blinking cursor
-    $("#console").html($("#console").html().substring(0, Typer.content().length - 1)); // remove it before adding the text
-  $("#console").append(Typer.text);
-  window.scrollBy(0, 50); // scroll to make sure bottom is always visible
-
-  var timer2 = setInterval( function t2() {
-    if (Typer.text && Typer.index > Typer.text.length) {
-      clearInterval(timer2);
-      $("#console").html($("#console").html().substring(0, Typer.content().length - Typer.text.length)); // clear before change directory
-      Typer.updLstChr();
-    }
-  }, 750);
-  
-  return true;
-}
