e650c8c284fc8832e501b8de2cba6338031bc2dd
Sotiris Roussis <xtonousou@gmail.com>
2017-02-26 03:34:45 +0200

    nothing works

--- js/inject.js
+++ js/inject.js
@@ -1,40 +1,40 @@
-window.onload = function() {
-  switch (document.title) {
+switch (document.title) {
 
-    case "/home/xtonousou":
-    
-      var bio = document.getElementById('cdh1');
-      var contact = document.getElementById('cdh2');
-      var projects = document.getElementById('cdh3');
-      var distro = document.getElementById('al');
-      var publickey = document.getElementById('pk');
+  case "/home/xtonousou":
+  
+    var bio = document.getElementById('cdh1');
+    var contact = document.getElementById('cdh2');
+    var projects = document.getElementById('cdh3');
+    var distro = document.getElementById('al');
+    var publickey = document.getElementById('pk');
 
-      bio.href = "/bio.html";
-      contact.href = "/contact.html";
-      projects.href = "/projects.html";
-      distro.href = "https://www.archlinux.org";
-      publickey.href = "/xtonousou-pubkey.asc";
+    bio.href = "/bio.html";
+    contact.href = "/contact.html";
+    projects.href = "/projects.html";
+    distro.href = "https://www.archlinux.org";
+    publickey.href = "/xtonousou-pubkey.asc";
 
+    $(document).ready(function() {
       bio.onclick = function() { appendCommand('cd bio/ '); }
       contact.onclick = function() { appendCommand('cd contact/ '); }
       projects.onclick = function() { appendCommand('cd projects/ '); }
       distro.onclick = function() { appendCommand('$BROWSER https://www.archlinux.org '); }
       publickey.onclick = function() { appendCommand('wget -q /xtonousou-pubkey.asc '); }
-      
-      break;
-    case "/home/xtonousou/bio":
+    });
+    
+    break;
+  case "/home/xtonousou/bio":
 
 
-      break;
-    case "/home/xtonousou/contact":
+    break;
+  case "/home/xtonousou/contact":
 
 
-      break;
-    case "/home/xtonousou/projects":
+    break;
+  case "/home/xtonousou/projects":
 
 
-      break;
-    default:
-      console.error("inject.js : unknown document.title");
-  }
+    break;
+  default:
+    console.error("inject.js : unknown document.title");
 }
